6d1f0f761246826efaa8e59e87846d916d6944d7
Kent McKinney <kent.p.mckinney@gmail.com>
2020-06-24 17:06:49 -0700

    add project template

---
+++ kpm-csharp-webform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kpm_csharp_webform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
