6e3f1be4947c069fc4278183871e3d783ea65141
Kent McKinney <kent.p.mckinney@gmail.com>
2020-06-24 20:16:24 -0700

    add libraries to support required features

--- kpm-csharp-webform/Controllers/HomeController.cs
+++ kpm-csharp-webform/Controllers/HomeController.cs
@@ -1,37 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using kpm_csharp_webform.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace kpm_csharp_webform.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public string Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      DotNetEnv.Env.Load();
+      string test = "<div>" + (string)System.Environment.GetEnvironmentVariable("TEST") + "</div>";
+      return test; //(IActionResult)
+                   // return View();
     }
+
+    public string Welcome(string name, int numTimes = 1)
+    {
+      return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+  }
 }
