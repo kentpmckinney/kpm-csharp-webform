eba81ad9a84e16220f27a800a03692a960d66020
Kent McKinney <kent.p.mckinney@gmail.com>
2020-06-25 23:05:50 -0700

    add form validation

--- kpm-csharp-webform/Controllers/HomeController.cs
+++ kpm-csharp-webform/Controllers/HomeController.cs
@@ -21,14 +21,10 @@ namespace kpm_csharp_webform.Controllers
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Index([FromForm(Name = "email")] string email, [FromForm(Name = "file")] IFormFile file)
     {
-      if (file != null)
+      if (file != null && file.Length > 0)
       {
         var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-
-        var path = Path.Combine(
-            Directory.GetCurrentDirectory(), "wwwroot",
-            fileName);
-        Console.WriteLine(path);
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", fileName);
 
         using (var stream = new FileStream(path, FileMode.Create))
         {
@@ -36,38 +32,44 @@ namespace kpm_csharp_webform.Controllers
         }
 
         string url = UploadObjectUsingPresignedURLTest.Upload(path);
+        string subject = $"URL for Uploaded File '{fileName}'";
+        string htmlBody = $@"
+          <ul>
+            <li>Current Time: {DateTime.Now.ToLongDateString()}</li>
+            <li>File Uploaded: {fileName}</li>
+          </ul>
+          <br/>
+          The uploaded file may be accessed <a href='{url}'>here</a> for the next 30 minutes.
+          <br/>
+          An email message with this information has been sent to <strong>{email}</strong>.
+        ";
+        string plainBody = $@"
+          Current Time: {DateTime.Now.ToLongDateString()}\n
+          File Uploaded: {fileName}\n\n
+          The uploaded file may be accessed for the next 30 minutes here: \n\n{url}\n\n
+          An email message with this information has been sent to '{email}'.
+        ";
 
-        //   SendEMail().Wait();
-        ViewData["Message"] = $@"
-            The uploaded file may be accessed <a href='{url}'>here</a> for the next 30 minutes.
-            An email message has been sent to <strong>{email}</strong>
-            with a link to the file's new cloud location.
-          ";
+        SendEMail(email, subject, plainBody, htmlBody).Wait();
+        ViewData["Message"] = htmlBody;
         return View("Success");
       }
       else
       {
-        ViewData["Message"] = $"Please select Browse and choose a file to upload.";
+        ViewData["Message"] = $@"
+          Please select Browse and choose a file to upload and try again.
+        ";
         return View("Failure");
       }
-
-
-
-
-
-
     }
 
-    static async Task SendEMail()
+    static async Task SendEMail(string toAddress, string subject, string plainBody, string htmlBody)
     {
       string apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
       SendGridClient client = new SendGridClient(apiKey);
       EmailAddress from = new EmailAddress("ken.p.mckinney@gmail.com", "Kent McKinney");
-      string subject = "Sending with SendGrid is Fun";
-      EmailAddress to = new EmailAddress("kent.p.mckinney@gmail.com", "Recipient");
-      string plainTextContent = "and easy to do anywhere, even with C#";
-      string htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-      SendGridMessage msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+      EmailAddress to = new EmailAddress(toAddress, "File Upload User");
+      SendGridMessage msg = MailHelper.CreateSingleEmail(from, to, subject, plainBody, htmlBody);
       SendGrid.Response response = await client.SendEmailAsync(msg);
     }
   }
